b005a33a4c3b9459636aec891b09f28ba6f295f9
zhenglian <zhenglian@globalegrow.com>
2019-07-04 19:18:09 +0800

    spring boot gradle plugin study

---
+++ src/main/java/com/zheng/springboot/HelloWorldApp.java
@@ -0,0 +1,23 @@
+package com.zheng.springboot;
+
+/**
+ * <pre>
+ *
+ *  File:
+ *
+ *  Copyright (c) 2016, globalegrow.com All Rights Reserved.
+ *
+ *  Description:
+ *  TODO
+ *
+ *  Revision History
+ *  Date,					Who,					What;
+ *  2019年07月04日			zhenglian			    Initial.
+ *
+ * </pre>
+ */
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
