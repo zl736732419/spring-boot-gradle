b32e0ccaf103c3ccb8b07ed72c594f21b8999847
z736732419 <736732419@qq.com>
2019-07-13 14:36:12 +0800

    添加redis

---
+++ src/main/java/com/zheng/springboot/redis/RedisController.java
@@ -0,0 +1,30 @@
+package com.zheng.springboot.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author zhenglian
+ * @Date 2019/7/13
+ */
+@RestController
+@RequestMapping("/redis")
+public class RedisController {
+    
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+    
+    @RequestMapping("/set/{key}/{value}")
+    public String put(@PathVariable("key") String key, @PathVariable("value") String value) {
+        redisTemplate.opsForValue().set(key, value);
+        return "OK";
+    }
+    
+    @RequestMapping("/get/{key}")
+    public String get(@PathVariable("key") String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+}
