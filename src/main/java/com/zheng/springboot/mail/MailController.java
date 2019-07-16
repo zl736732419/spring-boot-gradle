fec3b3b23ec4ae4b15d1b3fb09e82ab739ad3409
zhenglian <zhenglian@globalegrow.com>
2019-07-16 18:12:56 +0800

    修改邮件逻辑

---
+++ src/main/java/com/zheng/springboot/mail/MailController.java
@@ -0,0 +1,53 @@
+package com.zheng.springboot.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+/**
+ * <pre>
+ *
+ *  File:
+ *
+ *  Copyright (c) 2016, globalegrow.com All Rights Reserved.
+ *
+ *  Description:
+ *  TODO
+ *
+ *  Revision History
+ *  Date,					Who,					What;
+ *  2019年07月16日			zhenglian			    Initial.
+ *
+ * </pre>
+ */
+@RestController
+@RequestMapping("/mail")
+public class MailController {
+    
+    @Autowired
+    private JavaMailSender javaMailSender;
+    
+    @Value("${spring.mail.sender}")
+    private String sender;
+    
+    
+    @RequestMapping("/send/{email}/{content}")
+    public String send(@PathVariable("email") String receiver, @PathVariable("content") String content) throws Exception {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+        helper.setTo(receiver);
+        helper.setFrom(sender);
+        helper.setText("content: " + content);
+        // 添加附件
+        helper.addAttachment("测试文本文件.txt", new File("C:\\Users\\Administrator\\Desktop\\test.txt"));
+        javaMailSender.send(mimeMessage);
+        return "ok!";
+    }
+}
